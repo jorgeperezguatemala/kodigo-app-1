2ac89d110160cc3843678c894d9a62ed4e4b8a1e
Fernando Aguilar <feraguilar6985@gmail.com>
2023-07-24 19:41:01 -0600

    :sparkles: feat(bootstrap):add booststrap

--- views/bootcamp.view.php
+++ views/bootcamp.view.php
@@ -4,18 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kodigo App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Welcome to Kodigo app</h1>
-    <h2>All bootcamps</h2>
+    <h1 class="text-center">Welcome to Kodigo app</h1>
+    <h2 class="text-center">All bootcamps</h2>
 
-    <ul>
-        <?php foreach ($bootcamps as $bootcamp): ?>
-            <li>
-                <h2> <?php echo $bootcamp['title']; ?> </h2>
-                <p> <?php echo $bootcamp['description']; ?> </p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="container">
+        <div class="row">
+
+            <?php foreach ($bootcamps as $bootcamp): ?>
+
+                <div class="col-sm-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://blog.u-erre.mx/hubfs/funciones%20del%20desarrollador%20web.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $bootcamp['title']; ?></h5>
+                        <p class="card-text"><?php echo $bootcamp['description']; ?></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+
+                <?php endforeach; ?>
+            </div>
+    </div>
 </body>
 </html>
