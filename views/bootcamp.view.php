44a91a7cb55afe86876ef85fef176655fffc295e
Fernando Aguilar <feraguilar6985@gmail.com>
2023-07-25 18:10:36 -0600

    :sparkles: feat(templates):add templates

--- views/bootcamp.view.php
+++ views/bootcamp.view.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kodigo App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-</head>
-<body>
+<?php require('templates/Header.php'); ?>
     <h1 class="text-center">Welcome to Kodigo app</h1>
     <h2 class="text-center">All bootcamps</h2>
 
     <div class="container">
         <div class="row">
-
             <?php foreach ($bootcamps as $bootcamp): ?>
-
                 <div class="col-sm-3">
                 <div class="card" style="width: 18rem;">
                     <img src="https://blog.u-erre.mx/hubfs/funciones%20del%20desarrollador%20web.jpg" class="card-img-top" alt="...">
@@ -25,9 +15,7 @@
                     </div>
                     </div>
                 </div>
-
                 <?php endforeach; ?>
             </div>
     </div>
-</body>
-</html>
+<?php require('templates/Footer.php'); ?>
